a6821e492eac511fca4911e1297c74f07a621984
markluciano <marcus.o.martin@gmail.com>
2026-01-13 01:27:09 +0100

    Updated

---
+++ Random Module.py
@@ -0,0 +1,21 @@
+import random
+
+#random_integer = random.randint(1, 10)
+#print(random_integer)
+
+#random_number = random.random()
+#print(random_number)
+
+#random_float = random.uniform(1, 10)
+#print(random_float)
+
+#friends = ["Alice", "Bob", "Charlie", "David", "Larry"]
+#friends_integer = random.randint(0,4)
+
+#print(friends[friends_integer])
+
+user_name = input("What is your name?")
+
+print(user_name)
+
+
